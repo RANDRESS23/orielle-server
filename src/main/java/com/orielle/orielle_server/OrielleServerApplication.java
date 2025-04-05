750e0a0a6b5d066cf35a55527585c28d8925cc84
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-05 13:36:07 -0500

    feat: add spring boot initial configuration

---
+++ src/main/java/com/orielle/orielle_server/OrielleServerApplication.java
@@ -0,0 +1,13 @@
+package com.orielle.orielle_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrielleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrielleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orielle/orielle_server/OrielleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orielle.orielle_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrielleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
