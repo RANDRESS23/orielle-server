9b2b327fc70ed473bd362041b51e5dd1fdb1f34a
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-07 17:03:37 -0500

    feat: add product and category repositories

---
+++ src/main/java/com/orielle/orielle_server/adapters/driven/jpa/mysql/repository/ICategoryRepository.java
@@ -0,0 +1,14 @@
+package com.orielle.orielle_server.adapters.driven.jpa.mysql.repository;
+
+import com.orielle.orielle_server.adapters.driven.jpa.mysql.entity.CategoryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ICategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    Optional<CategoryEntity> findByName(String name);
+    Optional<CategoryEntity> findByCategoryId(Long categoryId);
+    Page<CategoryEntity> findAll(Pageable pageable);
+}
