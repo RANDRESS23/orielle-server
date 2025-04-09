b99c7559d3121754fb99b2376f2ae9ffd2962dd3
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-09 17:53:43 -0500

    feat: add new constants

--- src/main/java/com/orielle/orielle_server/adapters/driven/jpa/mysql/util/DrivenConstants.java
+++ src/main/java/com/orielle/orielle_server/adapters/driven/jpa/mysql/util/DrivenConstants.java
@@ -5,8 +5,8 @@ public class DrivenConstants {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String FIELD_NAME_NOT_BLANK_MESSAGE = "Field 'name' cannot be null.";
-    public static final String FIELD_DESCRIPTION_NOT_BLANK_MESSAGE = "Field 'description' cannot be null.";
+    public static final String FIELD_NAME_NOT_BLANK_MESSAGE = "Field 'name' can't be null.";
+    public static final String FIELD_DESCRIPTION_NOT_BLANK_MESSAGE = "Field 'description' can't be null.";
 
     public static final String CATEGORY_TABLE_NAME = "category";
     public static final String PRODUCT_TABLE_NAME = "product";
@@ -15,8 +15,67 @@ public class DrivenConstants {
     public static final String COLUMN_CART_CREATED_AT = "created_at";
     public static final String COLUMN_CART_UPDATED_AT = "updated_at";
 
+    public static final String CART_TABLE_NAME = "cart";
+    public static final String COLUMN_CART_ID = "cart_id";
+    public static final String COLUMN_CART_CLIENT_ID = "client_id";
+    public static final String COLUMN_CART_TOTAL_QUANTITY = "total_quantity";
+    public static final String COLUMN_CART_TOTAL_PRICE = "total_price";
+
+    public static final String CART_PRODUCT_TABLE_NAME = "cart_products";
+    public static final String COLUMN_CART_PRODUCT_ID = "cart_product_id";
+    public static final String COLUMN_CART_PRODUCT_PRODUCT_ID = "product_id";
+    public static final String COLUMN_CART_PRODUCT_NAME = "name";
+    public static final String COLUMN_CART_PRODUCT_QUANTITY = "quantity";
+
     public static final String FIELD_CATEGORY = "category";
 
     public static final String SORT_BY_PRODUCT_NAME = "name";
     public static final String SORT_BY_CATEGORY_NAME = "category.name";
+
+    public static final String DOCUMENT_REGEX = "^\\d+$";
+    public static final String PHONE_REGEX = "^\\+?\\d{1,13}$";
+    public static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+
+    public static final int MAXIMUM_USER_NAME_CHARACTERS = 50;
+    public static final int MAXIMUM_USER_LASTNAME_CHARACTERS = 50;
+    public static final int MAXIMUM_USER_PHONE_CHARACTERS = 13;
+    public static final int MAXIMUM_ROLE_DESCRIPTION_CHARACTERS = 200;
+
+    public static final String USERS_TABLE_NAME = "user";
+    public static final String COLUMN_USER_ID = "user_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_USER_LASTNAME = "lastname";
+    public static final String COLUMN_USER_DNI = "dni";
+    public static final String COLUMN_USER_PHONE = "phone";
+    public static final String COLUMN_USER_EMAIL = "email";
+    public static final String COLUMN_USER_PASSWORD = "password";
+    public static final String COLUMN_USER_BIRTHDATE = "birthdate";
+    public static final String COLUMN_ROLE_ID = "role_id";
+
+    public static final String ORDER_TABLE_NAME = "orders";
+    public static final String COLUMN_ORDER_ID = "order_id";
+    public static final String COLUMN_ORDER_CLIENT_ID = "client_id";
+    public static final String COLUMN_ORDER_TOTAL_QUANTITY = "total_quantity";
+    public static final String COLUMN_ORDER_TOTAL_PRICE = "total_price";
+
+    public static final String ORDER_ITEM_TABLE_NAME = "order_items";
+    public static final String COLUMN_ORDER_ITEM_ID = "order_item_id";
+    public static final String COLUMN_ORDER_ITEM_PRODUCT_ID = "product_id";
+    public static final String COLUMN_ORDER_ITEM_QUANTITY = "quantity";
+    public static final String COLUMN_ORDER_ITEM_TOTAL_PRICE = "total_price";
+
+    public static final String STATE_TABLE_NAME = "state";
+
+    public static final String ROLES_TABLE_NAME = "role";
+    public static final String COLUMN_ROLE_DESCRIPTION = "description";
+
+    public static final String USER_ID_FIELD = "userId";
+    public static final String ROLE_FIELD = "role";
+    public static final String FULL_NAME_FIELD = "fullName";
+
+    public static final String INVALID_DNI = "The 'dni' field is not valid";
+    public static final String INVALID_PHONE = "The 'phone' field is not valid";
+    public static final String INVALID_EMAIL = "The 'email' field is not valid";
+
+    public static final String AUTHORIZATION_HEADER  = "Authorization";
 }
--- src/main/java/com/orielle/orielle_server/domain/util/Constants.java
+++ src/main/java/com/orielle/orielle_server/domain/util/Constants.java
@@ -1,5 +1,7 @@
 package com.orielle.orielle_server.domain.util;
 
+import java.math.BigDecimal;
+
 public class Constants {
     private Constants() {
         throw new IllegalStateException("Utility class");
@@ -8,7 +10,10 @@ public class Constants {
     public static final String DNI_REGEX = "^\\d{7,10}$";
     public static final String PHONE_REGEX = "^\\+?\\d{1,12}$";
     public static final String EMAIL_REGEX = "^[\\w!#$%&'*+/=?`{|}~^.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+    public static final String PASSWORD_REGEX = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+])[A-Za-z\\d!@#$%^&*()_+]{8,}$";
     public static final Integer ZERO_CONSTANT = 0;
+    public static final Long QUANTITY_ZERO = 0L;
+    public static final BigDecimal TOTAL_PRICE_ZERO = new BigDecimal(0);
     public static final int MINIMUM_AGE_USER = 18;
     public static final int MAXIMUM_USER_NAME_CHARACTERS = 50;
     public static final int MAXIMUM_USER_LASTNAME_CHARACTERS = 50;
@@ -18,8 +23,11 @@ public class Constants {
     public static final int MAXIMUM_CATEGORY_NAME_CHARACTERS = 70;
     public static final int MAXIMUM_CATEGORY_DESCRIPTION_CHARACTERS = 120;
 
+    public static final String FIELD_USER_ID  = "userId";
     public static final String FIELD_NAME = "name";
     public static final String FIELD_CATEGORY = "category";
+    public static final String FIELD_DNI = "dni";
+    public static final String FIELD_EMAIL = "email";
 
     public static final String FIELD_NAME_NULL_MESSAGE = "Field 'name' cannot be null.";
     public static final String FIELD_LASTNAME_NULL_MESSAGE = "Field 'last name' cannot be null.";
@@ -60,10 +68,18 @@ public class Constants {
     public static final String MAXIMUM_CATEGORY_NAME_CHARACTERS_MESSAGE = "Category name can have up to 70 characters maximum.";
     public static final String MAXIMUM_CATEGORY_DESCRIPTION_CHARACTERS_MESSAGE = "Category description can have up to 120 characters maximum.";
 
-    public static final String INVALID_DOCUMENT = "The 'document' field is not valid.";
-    public static final String INVALID_PHONE = "The 'phone' field is not valid.";
-    public static final String INVALID_EMAIL = "The 'email' field is not valid.";
-    public static final String INVALID_AGE = "The 'birthdate' field is not valid, you must be of legal age.";
+    public static final String MINIMUM_USER_NAME_CHARACTERS_MESSAGE = "User name can't empty.";
+    public static final String MINIMUM_USER_LASTNAME_CHARACTERS_MESSAGE = "User lastname can't empty.";
+    public static final String MINIMUM_USER_DNI_CHARACTERS_MESSAGE = "User DNI can't empty.";
+    public static final String MINIMUM_USER_EMAIL_CHARACTERS_MESSAGE = "User email can't empty.";
+    public static final String MINIMUM_USER_PHONE_CHARACTERS_MESSAGE = "User phone can't empty.";
+    public static final String MINIMUM_USER_PASSWORD_CHARACTERS_MESSAGE = "User password can't empty.";
+
+    public static final String INVALID_DOCUMENT = "El campo 'documento' no es válido";
+    public static final String INVALID_PHONE = "El campo 'celular' no es válido";
+    public static final String INVALID_EMAIL = "El campo 'email' no es válido";
+    public static final String INVALID_AGE = "El campo 'fecha de nacimiento' no es válido, debes ser mayor de edad";
+    public static final String INVALID_PASSWORD = "El campo 'contraseña' no es válido, debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial";
 
     public static final String DOCUMENT_ALREADY_EXISTS_MESSAGE = "Document already exists.";
     public static final String EMAIL_ALREADY_EXISTS_MESSAGE = "Email already exists.";
@@ -71,6 +87,16 @@ public class Constants {
 
     public static final String INVALID_CREDENTIALS_MESSAGE = "Invalid credentials.";
     public static final String INVALID_PARAM_MESSAGE = "Invalid parameter.";
+
+    public static final String CART_ALREADY_EXISTS_MESSAGE = "Cart already exists.";
+    public static final String CART_NOT_FOUND = "Cart not found.";
+    public static final String EMPTY_CART_MESSAGE = "No products were found in the cart.";
+    public static final String INVALID_ADD_PRODUCT_QUANTITY_MESSAGE = "La cantidad del producto que desea agregar al carrito excede la cantidad de stock disponible para el producto.";
+    public static final String INVALID_BUY_PRODUCT_QUANTITY_MESSAGE = "La cantidad del producto que desea comprar excede la cantidad de stock disponible para el producto.";
+    public static final String INVALID_REMOVE_PRODUCT_QUANTITY_FROM_CART_MESSAGE = "La cantidad del producto que desea eliminar del carrito excede la cantidad del producto que está en el carrito.";
+    public static final String CART_PRODUCT_NOT_FOUND = "The product was not found in the cart.";
+
+    public static final String DEFAULT_CATEGORY_PARAM = "all";
 
     public static final String NEGATIVE_NOT_ALLOWED_EXCEPTION_MESSAGE = "Negative exception not allowed.";
 
