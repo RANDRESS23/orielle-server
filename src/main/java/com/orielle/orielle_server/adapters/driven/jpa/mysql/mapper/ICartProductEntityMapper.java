7baa6c83383816fcad5b6c396b6f351209477c49
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-09 12:36:40 -0500

    feat: add cart and cart product entity mappers

---
+++ src/main/java/com/orielle/orielle_server/adapters/driven/jpa/mysql/mapper/ICartProductEntityMapper.java
@@ -0,0 +1,23 @@
+package com.orielle.orielle_server.adapters.driven.jpa.mysql.mapper;
+
+import com.orielle.orielle_server.adapters.driven.jpa.mysql.entity.CartProductEntity;
+import com.orielle.orielle_server.domain.model.CartProduct;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ICartProductEntityMapper {
+    CartProductEntity toEntity(CartProduct cartProduct);
+    CartProduct toDomainModel(CartProductEntity cartProductEntity);
+    List<CartProduct> toListOfCartProducts(List<CartProductEntity> listOfCartProductsEntity);
+
+    default Page<CartProduct> toPageOfCartProducts(Page<CartProductEntity> pageOfCartProductsEntity) {
+        List<CartProduct> dtoList = pageOfCartProductsEntity.getContent().stream()
+                .map(this::toDomainModel)
+                .toList();
+        return new PageImpl<>(dtoList, pageOfCartProductsEntity.getPageable(), pageOfCartProductsEntity.getTotalElements());
+    }
+}
