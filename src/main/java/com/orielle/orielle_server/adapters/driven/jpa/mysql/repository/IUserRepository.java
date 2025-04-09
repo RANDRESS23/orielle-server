4c2364701bcc91ef4727a2dc1628e140e3e2ad66
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-04-09 14:46:48 -0500

    feat: add user and role repositories

---
+++ src/main/java/com/orielle/orielle_server/adapters/driven/jpa/mysql/repository/IUserRepository.java
@@ -0,0 +1,19 @@
+package com.orielle.orielle_server.adapters.driven.jpa.mysql.repository;
+
+import com.orielle.orielle_server.adapters.driven.jpa.mysql.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByDocument(String document);
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByPhone(String phone);
+
+    @Query(value = "SELECT * FROM user u WHERE u.role_id = :roleId", nativeQuery = true)
+    Page<UserEntity> findAllClients(@Param("roleId") Long roleId, Pageable pageable);
+}
